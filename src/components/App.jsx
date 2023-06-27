39a06c9a6c288fc963b7b625cdd013b587490eff
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-27 18:56:33 +0300

    updt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
 import "../styles/main.css"
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
-import Home from "pages/Home";
-import Projects from "pages/Projects";
+// import Home from "pages/Home";
+// import Projects from "pages/Projects";
 import Contacts from "pages/Contacts";
 
-export const App = () => {
+function App() {
   return (
     <div>
 
       <Navbar />
-
+{/* 
       <Home />
 
-      <Projects />
+      <Projects /> */}
 
       <Contacts />
 
@@ -22,3 +22,5 @@ export const App = () => {
     </div>
   );
 };
+
+export default App;
