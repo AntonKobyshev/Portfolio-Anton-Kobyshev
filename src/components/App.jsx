faca9f49854163b4110c26e3f433b161866e5ef8
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-27 16:20:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import "../styles/main.css"
+import sun from "../img/icons/sun.svg"
+import moon from "../img/icons/moon.svg"
 
 export const App = () => {
   return (
@@ -9,8 +11,8 @@ export const App = () => {
                 <a href="./index.html" className="logo"><strong>Frontend developer</strong> portfolio</a>
 
                 <button className="dark-mode-btn">
-                    <img src="./img/icons/sun.svg" alt="Light mode" className="dark-mode-btn__icon" />
-                    <img src="./img/icons/moon.svg" alt="Dark mode" className="dark-mode-btn__icon" />
+                    <img src={ sun } alt="Light mode" className="dark-mode-btn__icon" />
+                    <img src={ moon } alt="Dark mode" className="dark-mode-btn__icon" />
                 </button>
 
                 <ul className="nav-list">
