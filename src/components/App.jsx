2b2e27f58bde8698b34c1dafd2dfaffa8a82c470
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-27 16:34:18 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,7 +30,7 @@ export const App = () => {
         <div className="header__wrapper">
             <h1 className="header__title">
                 <strong>Hi, my name is <em>Anton</em></strong><br />
-                I am a frontend developer
+                I am a fullstack developer
             </h1>
             <div className="header__text">
                 <p>with passion for learning and creating.</p>
