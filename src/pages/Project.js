f50af32d6cd190a38c5cbff16ce5736959c03275
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-28 18:37:47 +0300

    updt

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,28 +1,42 @@
-import BtnGitHub from "components/btnGitHub/BtnGitHub";
-import img from "./../img/projects/01-big.jpg"
+import { useParams } from 'react-router-dom';
+import BtnGitHub from 'components/btnGitHub/BtnGitHub';
+import BtnLink from 'components/btnLink/BtnLink';
+import { projects } from 'helpers/projectsList';
 
 const Project = () => {
-    return ( 
+  const { id } = useParams();
+  const project = projects.find(project => project.id === parseInt(id));
+
+  console.log(project);
+
+  return (
     <main className="section">
-        <div className="container">
-            <div className="project-details">
+      <div className="container">
+        <div className="project-details">
+          <h1 className="title-1">{project.title}</h1>
 
-                <h1 className="title-1">Video service</h1>
+          <img
+            src={project.imgBig}
+            alt="{project.title}"
+            className="project-details__cover"
+          />
 
-                <img src={img} alt="" className="project-details__cover" />
+          <div className="project-details__desc">
+            <p>Skills: {project.skills}</p>
+            <p>
+              Description:
+              <span className="project-details__desc--lite">
+                {project.description}
+              </span>
+            </p>
+          </div>
+          <BtnLink link={project.link} />
 
-                <div className="project-details__desc">
-                    <p>Skills: React, Node.js, MongoDB</p>
-                </div>
-
-                <BtnGitHub link="https://github.com/AntonKobyshev/goit-react-hw-04-phonebook" />
-             
-
-            </div>
+          <BtnGitHub link={project.gitHubLink} />
         </div>
+      </div>
     </main>
+  );
+};
 
-     );
-}
- 
 export default Project;
