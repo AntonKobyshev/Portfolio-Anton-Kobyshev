76cedc50ae274a42164ffe3284b20d0c9c422135
Anton Kobyshev <antonkobyshev@gmail.com>
2023-10-17 17:35:43 +0300

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,6 @@
 import './style.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -14,14 +16,19 @@ const Header = () => {
         <div className="header__text">
           <p>with a passion to create.</p>
         </div>
-        <a
-          href="https://drive.google.com/file/d/1MLtCq15eL_XeS43IOKtmn8Ks2oGpHbD0/view?usp=sharing"
-          className="btn"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Download CV
-        </a>
+        <div className="links">
+          <Link to="/projects">
+            <button className="btn">Go to Projects</button>
+          </Link>
+          <a
+            href="https://drive.google.com/file/d/1MLtCq15eL_XeS43IOKtmn8Ks2oGpHbD0/view?usp=sharing"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Download CV
+          </a>
+        </div>
       </div>
     </header>
   );
